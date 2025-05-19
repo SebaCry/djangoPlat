d4c1a84d1c979ad14818dca9058d2a866699e7c4
sebacry3 <Jspc513@gmail.com>
2025-05-18 21:27:58 -0500

    i understand the models relationals of db

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -16,10 +16,19 @@ class Publisher(models.Model):
     def __str__(self):
         return self.name
     
+class Author(models.Model):
+    name = models.TextField(max_length=25)
+    birth_day = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
 class Book(models.Model):
     title = models.TextField(max_length=25)
     publication_date = models.DateField()
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author,related_name="authors")
 
     def __str__(self):
         return self.title
+
