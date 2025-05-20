c2ad1233972cad5cce666eb1c9a293eb2072c5e6
sebacry3 <Jspc513@gmail.com>
2025-05-20 08:36:21 -0500

    I create new view and templaet using Class OPP

--- djangoCoffe/productos/models.py
+++ djangoCoffe/productos/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     description = models.TextField(max_length=150,verbose_name='descripción')
     price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='precio')
     available = models.BooleanField(default=True, verbose_name='disponible')
-    photo = models.ImageField(upload_to='logos', null=True, blank=True, verbose_name='foto')
+    photo = models.ImageField(upload_to='productos', null=True, blank=True, verbose_name='foto')
 
     def __str__(self):
         return self.name
--- djangoCoffe/productos/views.py
+++ djangoCoffe/productos/views.py
@@ -1,3 +1,16 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
+from .models import Product
+
+class ProductTemplateView(TemplateView):
+    template_name = "productos/productos.html"
+
+    def get_context_data(self):
+        productos = Product.objects.all()
+
+        return {
+            "productos" : productos
+        }
+
 
 # Create your views here.
