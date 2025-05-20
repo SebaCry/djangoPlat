3a8900709a5e19be665d7f144e7d86246e19334f
sebacry3 <Jspc513@gmail.com>
2025-05-20 11:09:27 -0500

    finish the modify files and folders of tailwind and UI

--- djangoCoffe/productos/urls.py
+++ djangoCoffe/productos/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('productos/', views.ProductTemplateView.as_view())
+    path('agregar/', views.ProductFormView.as_view(), name="add_product"),
+    path('', views.ProductListView.as_view(), name="productos")
 ]
+
+
--- djangoCoffe/productos/views.py
+++ djangoCoffe/productos/views.py
@@ -1,16 +1,25 @@
 from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views import generic
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 
-class ProductTemplateView(TemplateView):
+class ProductFormView(generic.FormView):
+    template_name = "productos/add_producto.html"
+    form_class = ProductForm
+    success_url = reverse_lazy('productos')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+class ProductListView(generic.ListView):
+    model = Product
     template_name = "productos/productos.html"
+    context_object_name = 'productos'
 
-    def get_context_data(self):
-        productos = Product.objects.all()
 
-        return {
-            "productos" : productos
-        }
 
 
 # Create your views here.
